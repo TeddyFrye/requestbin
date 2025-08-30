1c0af757e7727d1cab676f1a14a2537d21b93ec5
Amar Thakor <amar@Mac.home>
2025-08-30 16:45:14 -0700

    Set up frontend skeleton

---
+++ frontend/src/components/BasketPage.jsx
@@ -0,0 +1,5 @@
+const BasketPage = () => {
+  return <p>This is the basket component</p>;
+};
+
+export default BasketPage;
---
+++ frontend/src/components/MainPage.jsx
@@ -0,0 +1,5 @@
+const MainPage = () => {
+  return <p>This is the main component</p>;
+};
+
+export default MainPage;
