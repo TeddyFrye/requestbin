410315b4c7ce4c7a2469d0f5a9ef9455a86f7c48
Caleb Soong <soong.caleb@gmail.com>
2025-09-03 21:15:45 -0500

    added to backend and db files

--- backend/index.js
+++ backend/index.js
@@ -8,43 +8,50 @@ app.use(cors());
 app.use(express.json());
 //View all baskets
 app.get('/api/baskets', async (request, response) => {
-  let baskets = await PgPersistence.listBaskets();
+  const baskets = await PgPersistence.listBaskets();
+  
   response.json(baskets);
 })
 //Add a new basket
 app.post('/api/baskets', async (request, response) => {
-  let name = generateName();
-  let newBasket = await PgPersistence.createBasket(name);
+  const name = generateName();
+  const newBasket = await PgPersistence.createBasket(name);
+
   response.json(newBasket);
 })
 //View a specific basket
 app.get('/api/baskets/:name', async (request, response) => {
   const name = request.params.name;
-  let basket = await PgPersistence.getBasket(name);
+  const requests = await PgPersistence.listRequests(name);
 
-  if (basket) {
-    response.json(basket);
+  if (requests) {
+    response.json(requests);
   } else {
     response.status(404).end();
   }
-
-  console.log(basket);
-})
+});
 //Delete the current basket
-app.delete('/api/baskets/:name', (request, response) => {
+app.delete('/api/baskets/:name', async (request, response) => {
   const name = request.params.name;
-  baskets = baskets.filter(basket => basket.name !== name);
+
+  await MongoDB.deleteRequestsByBasketId(name);
+  await PgPersistence.deleteRequests(name);
+  await PgPersistence.deleteBasket(name);
+
   response.status(204).end();
 });
 //Delete all requests in the current basket
-app.delete('/api/baskets/:name/requests', (request, response) => {
+app.delete('/api/baskets/:name/requests', async (request, response) => {
   const name = request.params.name;
-  const basket = baskets.find(basket => basket.name === name);
-  basket.requests = [];
+
+  await MongoDB.deleteRequestsByBasketId(name);
+  await PgPersistence.deleteRequests(name);
+
   response.status(204).end();
 });
 
 const PORT = 3001;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
