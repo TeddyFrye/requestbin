2a870134df28585810647c07af0f6f1d1c712dea
Caleb Soong <soong.caleb@gmail.com>
2025-09-03 22:12:19 -0500

    added basket id to requests in mongo, other changes

--- backend/index.js
+++ backend/index.js
@@ -6,22 +6,49 @@ const MongoDB = require("./db/mongo");
 
 app.use(cors());
 app.use(express.json());
+//Endpoints
+app.all('/:name', async (request, response) => {
+  const name = request.params.name;
+  const basket = await PgPersistence.getBasket(name);
+
+  if (!basket) {
+    response.status(404).end();
+  }
+
+  const { body, path, query, method, headers } = request;
+  const id = await MongoDB.storeRequest(body, name);
+
+  await PgPersistence.insertRequest({
+    basketId: name,
+    path,
+    query,
+    method,
+    headers: JSON.stringify(headers),
+    bodyMongoId: id,
+  });
+});
 //View all baskets
 app.get('/api/baskets', async (request, response) => {
   const baskets = await PgPersistence.listBaskets();
-  
+
   response.json(baskets);
-})
+});
 //Add a new basket
 app.post('/api/baskets', async (request, response) => {
   const name = generateName();
   const newBasket = await PgPersistence.createBasket(name);
 
   response.json(newBasket);
-})
+});
 //View a specific basket
 app.get('/api/baskets/:name', async (request, response) => {
   const name = request.params.name;
+  const basket = await PgPersistence.getBasket(name);
+
+  if (!basket) {
+    response.status(404).end();
+  }
+
   const requests = await PgPersistence.listRequests(name);
 
   if (requests) {
@@ -30,22 +57,31 @@ app.get('/api/baskets/:name', async (request, response) => {
     response.status(404).end();
   }
 });
-//Delete the current basket
-app.delete('/api/baskets/:name', async (request, response) => {
-  const name = request.params.name;
-
-  await MongoDB.deleteRequestsByBasketId(name);
-  await PgPersistence.deleteRequests(name);
-  await PgPersistence.deleteBasket(name);
-
-  response.status(204).end();
-});
 //Delete all requests in the current basket
 app.delete('/api/baskets/:name/requests', async (request, response) => {
   const name = request.params.name;
+  const basket = await PgPersistence.getBasket(name);
+
+  if (!basket) {
+    response.status(404).end();
+  }
 
   await MongoDB.deleteRequestsByBasketId(name);
   await PgPersistence.deleteRequests(name);
+
+  response.status(204).end();
+});
+//Delete the current basket
+app.delete('/api/baskets/:name', async (request, response) => {
+  const name = request.params.name;
+  const basket = await PgPersistence.getBasket(name);
+
+  if (!basket) {
+    response.status(404).end();
+  }
+
+  await MongoDB.deleteRequestsByBasketId(name);
+  await PgPersistence.deleteBasket(name);
 
   response.status(204).end();
 });
@@ -54,7 +90,7 @@ const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-})
+});
 
 function generateName() {
   const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
