686502e2409bb033449f0385a6574f199b49eaa9
narjoDev <63086420+narjoDev@users.noreply.github.com>
2025-09-10 14:44:44 -0700

    add region to AWS secrets manager

--- backend/aws-secrets.js
+++ backend/aws-secrets.js
@@ -4,7 +4,7 @@ import {
 } from "@aws-sdk/client-secrets-manager";
 
 export const getSecretValue = async (secretName) => {
-  const client = new SecretsManagerClient();
+  const client = new SecretsManagerClient({ region: "us-east-1" });
   const response = await client.send(
     new GetSecretValueCommand({
       SecretId: secretName,
